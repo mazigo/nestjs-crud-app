627d9d76ffd32b01704b93cb52fb2b78b3c3cc1f
jmazigo <jmazigo@gmail.com>
2022-06-06 16:36:31 +0300

    todos service created

---
+++ src/services/todos.service.ts
@@ -0,0 +1,47 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import CreateTodoDto from 'src/dtos/createTodo.dto';
+import UpdateTodoDto from 'src/dtos/updateTodo.dto';
+import Todo from 'src/entities/todos.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class TodosService {
+  constructor(@InjectRepository(Todo) private todoRepo: Repository<Todo>) {}
+  //get all
+  getAllTodos() {
+    return this.todoRepo.find();
+  }
+  //find by id
+  async getById(id: number) {
+    const todo = await this.todoRepo.findOne(id);
+    if (todo) {
+      return todo;
+    }
+    throw new HttpException('Todo not found', HttpStatus.NOT_FOUND);
+  }
+  //create a new
+  async createTodo(todo: CreateTodoDto) {
+    const newTodo = this.todoRepo.create(todo);
+    await this.todoRepo.save(newTodo);
+    return newTodo;
+  }
+  // update
+  async updateTodo(id: number, post: UpdateTodoDto) {
+    await this.todoRepo.update(id, post);
+    const updatedTodo = await this.todoRepo.findOne(id);
+    if (updatedTodo) {
+      return updatedTodo;
+    }
+
+    throw new HttpException('Todo not found', HttpStatus.NOT_FOUND);
+  }
+
+  // delete
+  async deleteTodo(id: number) {
+    const deletedTodo = await this.todoRepo.delete(id);
+    if (!deletedTodo.affected) {
+      throw new HttpException('Todo not found', HttpStatus.NOT_FOUND);
+    }
+  }
+}
