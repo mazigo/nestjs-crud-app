1299fd096c79231ad736abf65f55342ae78e7162
jmazigo <jmazigo@gmail.com>
2022-06-06 15:22:38 +0300

    todos dtos created

---
+++ src/dtos/createTodo.dto.ts
@@ -0,0 +1,6 @@
+export class CreateTodoDto {
+  title: string;
+  description: string;
+  status: boolean;
+}
+export default CreateTodoDto;
---
+++ src/dtos/updateTodo.dto.ts
@@ -0,0 +1,7 @@
+export class UpdateTodoDto {
+  id: number;
+  title: string;
+  description: string;
+  status: boolean;
+}
+export default UpdateTodoDto;
